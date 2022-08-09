3fbac4b8f952e898402df1bfa8d7ac5f0559032f
hklim00 <g5bugi82@gmail.com>
2022-08-09 10:18:43 +0900

    axio 설치 json 파일 불러기

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -1,9 +1,37 @@
 import Layout from '../common/Layout';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Department() {
+	const [Members, setMembers] = useState([]);
+
+	useEffect(() => {
+		// fetch(process.env.PUBLIC_URL + '/DB/members.json')
+		// 	.then((data) => data.json())
+		// 	.then((json) => console.log(json));
+		axios
+			.get(process.env.PUBLIC_URL + '/DB/members.json')
+			.then((json) => setMembers(json.data.members));
+	}, []);
+
 	return (
 		<Layout name={'Department'}>
-			<p>Department</p>
+			{Members.map((member, idx) => {
+				return (
+					<article key={idx}>
+						<div className='inner'>
+							<div className='pic'>
+								<img
+									src={`${process.env.PUBLIC_URL}/img/${member.pic}`}
+									alt=''
+								/>
+							</div>
+							<h2>{member.name}</h2>
+							<p>{member.position}</p>
+						</div>
+					</article>
+				);
+			})}
 		</Layout>
 	);
 }
