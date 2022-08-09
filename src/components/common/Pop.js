1b74d877ad136bd9e5fdad9ea71db11f02602e42
hklim00 <g5bugi82@gmail.com>
2022-08-09 15:53:49 +0900

    youtube popup

---
+++ src/components/common/Pop.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Pop({ children, setOpen }) {
+	return (
+		<aside className='pop'>
+			<div className='con'>{children}</div>
+			<span className='close' onClick={() => setOpen(false)}>
+				close
+			</span>
+		</aside>
+	);
+}
+
+export default Pop;
--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -1,9 +1,15 @@
 import Layout from '../common/Layout';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
+import Pop from '../common/Pop';
 
 function Youtube() {
+	const line = useRef(null);
 	const [Vids, setVids] = useState([]);
+	const [Open, setOpen] = useState(false);
+	const [Index, setIndex] = useState(0);
 
 	useEffect(() => {
 		const key = 'AIzaSyBDL1S8asY8CR73ihG02orQB3BdWha5F1A';
@@ -17,20 +23,43 @@ function Youtube() {
 	}, []);
 
 	return (
-		<Layout name={'Youtube'}>
-			{Vids.map((vid) => (
-				<article key={vid.id}>
-					<h2>{vid.snippet.title}</h2>
-					<div className='txt'>
-						<p>{vid.snippet.description}</p>
-						<span>{vid.snippet.publishedAt}</span>
-					</div>
-					<div className='pic'>
-						<img src={vid.snippet.thumbnails.high.url} alt='' />
-					</div>
-				</article>
-			))}
-		</Layout>
+		<>
+			<Layout name={'Youtube'}>
+				{Vids.map((vid, idx) => {
+					let tit = vid.snippet.title;
+					let des = vid.snippet.description;
+					let date = vid.snippet.publishedAt;
+					return (
+						<article key={vid.id}>
+							<h2>{tit.length > 30 ? tit.substr(0, 30) + '...' : tit}</h2>
+							<div className='txt'>
+								<p>{des.length > 200 ? des.substr(0, 200) + '...' : des}</p>
+								<span>{date.split('T')[0]}</span>
+							</div>
+							<div className='pic'>
+								<img src={vid.snippet.thumbnails.high.url} alt='' />
+								<FontAwesomeIcon
+									icon={faYoutube}
+									ref={line}
+									onClick={() => {
+										setOpen(true);
+										setIndex(idx);
+									}}
+								/>
+							</div>
+						</article>
+					);
+				})}
+			</Layout>
+			{Open && (
+				<Pop setOpen={setOpen}>
+					<iframe
+						title='video'
+						src={`https://www.youtube.com/embed/${Vids[Index].snippet.resourceId.videoId}`}
+						frameBorder='0'></iframe>
+				</Pop>
+			)}
+		</>
 	);
 }
 
