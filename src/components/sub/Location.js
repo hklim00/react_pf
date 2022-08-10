3535d51c31d71130b64780d0eee9ce98e6b3371c
hklim00 <g5bugi82@gmail.com>
2022-08-10 16:09:53 +0900

    kakao api 가져오기

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -1,9 +1,22 @@
 import Layout from '../common/Layout';
 
+import { useRef, useEffect, useState } from 'react';
+
 function Location() {
+	const { kakao } = window; // window 객체 안에서 kakao 키값 가져오기
+	const container = useRef(null);
+	const option = {
+		center: new kakao.maps.LatLng(33.450701, 126.570667),
+		level: 3,
+	};
+
+	useEffect(() => {
+		const map = new kakao.maps.Map(container.current, option);
+	}, []);
+
 	return (
 		<Layout name={'Location'}>
-			<p>Location</p>
+			<div id='map' ref={container}></div>
 		</Layout>
 	);
 }
