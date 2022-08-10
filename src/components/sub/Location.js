cd3de097f8d7ccfa467bdab3f75475b719ad19af
hklim00 <g5bugi82@gmail.com>
2022-08-10 16:33:51 +0900

    지도 불러오기

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -9,9 +9,14 @@ function Location() {
 		center: new kakao.maps.LatLng(33.450701, 126.570667),
 		level: 3,
 	};
+	const markerPosition = option.center;
+	const marker = new kakao.maps.Marker({
+		position: markerPosition,
+	});
 
 	useEffect(() => {
 		const map = new kakao.maps.Map(container.current, option);
+		marker.setMap(map);
 	}, []);
 
 	return (
