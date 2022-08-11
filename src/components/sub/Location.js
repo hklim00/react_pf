fb5b4e99d1c8750f8ce6b1f83bef671a51e9467d
hklim00 <g5bugi82@gmail.com>
2022-08-11 10:47:13 +0900

    트래픽버튼 토글, 지도정보 객체화

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -3,18 +3,32 @@ import Layout from '../common/Layout';
 import { useRef, useEffect, useState } from 'react';
 
 function Location() {
-	const [Location, setLocation] = useState(null);
 	const { kakao } = window; // window 객체 안에서 kakao 키값 가져오기
 	const container = useRef(null);
+
+	const info = [
+		{
+			title: '삼성동 코엑스',
+			latLng: new kakao.maps.LatLng(37.51271224560607, 127.06069135102807),
+			imgUrl: process.env.PUBLIC_URL + '/img/marker1.png',
+			imgSize: new kakao.maps.Size(232, 99),
+			imgPos: { offset: new kakao.maps.Point(116, 99) },
+		},
+	];
+
+	const [Location, setLocation] = useState(null);
+	const [Traffic, setTraffic] = useState(false);
+	const [Info, setInfo] = useState(info);
+
 	const option = {
-		center: new kakao.maps.LatLng(37.51271224560607, 127.06069135102807),
+		center: Info[0].latLng,
 		level: 3,
 	};
 	const markerPosition = option.center;
 
-	const imgSrc = process.env.PUBLIC_URL + '/img/marker1.png';
-	const imgSize = new kakao.maps.Size(232, 99);
-	const imgOption = { offset: new kakao.maps.Point(116, 99) };
+	const imgSrc = Info[0].imgUrl;
+	const imgSize = Info[0].imgSize;
+	const imgOption = Info[0].imgPos;
 
 	const markerImage = new kakao.maps.MarkerImage(imgSrc, imgSize, imgOption);
 
@@ -29,20 +43,18 @@ function Location() {
 		setLocation(map_instance);
 	}, []);
 
+	useEffect(() => {
+		if (!Location) return;
+		Traffic
+			? Location.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
+			: Location.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+	}, [Traffic]);
+
 	return (
 		<Layout name={'Location'}>
 			<div id='map' ref={container}></div>
-			<button
-				onClick={() =>
-					Location.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
-				}>
-				Traffic On
-			</button>
-			<button
-				onClick={() =>
-					Location.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
-				}>
-				Traffic Off
+			<button onClick={() => setTraffic(!Traffic)}>
+				{Traffic ? 'Traffic Off' : 'Traffic On'}
 			</button>
 		</Layout>
 	);
