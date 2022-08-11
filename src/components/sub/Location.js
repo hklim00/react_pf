b64f18fdcbb7efd67eaf76c45ee22a2a179c5a12
hklim00 <g5bugi82@gmail.com>
2022-08-11 10:07:42 +0900

    클릭 시  정보량 표시

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -3,23 +3,20 @@ import Layout from '../common/Layout';
 import { useRef, useEffect, useState } from 'react';
 
 function Location() {
+	const [Location, setLocation] = useState(null);
 	const { kakao } = window; // window 객체 안에서 kakao 키값 가져오기
 	const container = useRef(null);
 	const option = {
-		center: new kakao.maps.LatLng(33.450701, 126.570667),
+		center: new kakao.maps.LatLng(37.51271224560607, 127.06069135102807),
 		level: 3,
 	};
 	const markerPosition = option.center;
 
-	const imageSrc = process.env.PUBLIC_URL + '/img/marker1.png';
-	const imageSize = new kakao.maps.Size(232, 99);
-	const imageOption = { offset: new kakao.maps.Point(116, 99) };
+	const imgSrc = process.env.PUBLIC_URL + '/img/marker1.png';
+	const imgSize = new kakao.maps.Size(232, 99);
+	const imgOption = { offset: new kakao.maps.Point(116, 99) };
 
-	const markerImage = new kakao.maps.MarkerImage(
-		imageSrc,
-		imageSize,
-		imageOption
-	);
+	const markerImage = new kakao.maps.MarkerImage(imgSrc, imgSize, imgOption);
 
 	const marker = new kakao.maps.Marker({
 		position: markerPosition,
@@ -27,13 +24,26 @@ function Location() {
 	});
 
 	useEffect(() => {
-		const map = new kakao.maps.Map(container.current, option);
-		marker.setMap(map);
+		const map_instance = new kakao.maps.Map(container.current, option);
+		marker.setMap(map_instance);
+		setLocation(map_instance);
 	}, []);
 
 	return (
 		<Layout name={'Location'}>
 			<div id='map' ref={container}></div>
+			<button
+				onClick={() =>
+					Location.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
+				}>
+				Traffic On
+			</button>
+			<button
+				onClick={() =>
+					Location.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
+				}>
+				Traffic Off
+			</button>
 		</Layout>
 	);
 }
