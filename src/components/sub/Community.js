11bb0c08594ba610b33f877fb684b9ef6d9ffa6f
hklim00 <g5bugi82@gmail.com>
2022-08-12 17:35:09 +0900

    value state생성, onChange event 생성

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -1,9 +1,67 @@
 import Layout from '../common/Layout';
+import { useRef, useState } from 'react';
 
 function Community() {
+	const input = useRef(null);
+	const textarea = useRef(null);
+
+	const [Posts, setPosts] = useState([]);
+
+	const resetForm = () => {
+		input.current.value = '';
+		textarea.current.value = '';
+	};
+
+	const createPost = () => {
+		if (!input.current.value.trim() || !textarea.current.value.trim()) {
+			return;
+		}
+		setPosts([
+			...Posts,
+			{
+				title: input.current.value,
+				content: textarea.current.value,
+			},
+		]);
+
+		resetForm();
+	};
+
+	const deletePost = (index) => {
+		const newPosts = Posts.filter((_, idx) => idx !== index);
+		setPosts(newPosts);
+	};
+
 	return (
 		<Layout name={'Community'}>
-			<p>Community</p>
+			<div className='inputBox'>
+				<input type='text' placeholder='제목을 입력하세요' ref={input} />
+				<br />
+				<textarea
+					name=''
+					id=''
+					cols='30'
+					rows='3'
+					placeholder='제목을 입력하세요'
+					ref={textarea}></textarea>
+				<br />
+				<button onClick={resetForm}>CANCEL</button>
+				<button onClick={createPost}>WRITE</button>
+			</div>
+			<div className='showBox' style={{ height: 200 }}>
+				{Posts.map((val, idx) => {
+					return (
+						<article key={idx}>
+							<h2>{val.title}</h2>
+							<p>{val.content}</p>
+							<div className='btnSet'>
+								<button>EDIT</button>
+								<button onClick={() => deletePost(idx)}>DELETE</button>
+							</div>
+						</article>
+					);
+				})}
+			</div>
 		</Layout>
 	);
 }
--- src/components/sub/Members.js
+++ src/components/sub/Members.js
@@ -3,124 +3,28 @@ import { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
 function Members() {
-	const history = useHistory();
+	// 입력값 담을 초기 객체 생성
 	const initVal = {
 		userid: '',
-		email: '',
 		pwd1: '',
-		pwd2: '',
-		gender: null,
-		interests: null,
-		edu: '',
+		pewd2: '',
+		email: '',
+		gender: false,
+		interests: false,
+		edu: false,
 		comments: '',
 	};
+
 	const [Val, setVal] = useState(initVal);
-	const [Err, setErr] = useState({});
-	//전송 버튼 클릭 유무를 담을 스테이트
-	const [Submit, setSubmit] = useState(false);
-
-	const check = (value) => {
-		const errs = {};
-
-		//인증처리할 조건 정규표현식
-		const eng = /[a-zA-Z]/;
-		const num = /[0-9]/;
-		const spc = /[~!@#$%^&*()_+\]]/;
-
-		//userid 인증처리
-		if (value.userid.length < 5) {
-			errs.userid = '아이디를 5글자 이상 입력하세요';
-		}
-		//email 인증처리
-		if (value.email.length < 8 || !/@/.test(value.email)) {
-			errs.email = '이메일주소는 8글자 이상 @를 포함하세요';
-		}
-		//pwd1 인증처리
-		if (
-			value.pwd1.length < 6 ||
-			!eng.test(value.pwd1) ||
-			!num.test(value.pwd1) ||
-			!spc.test(value.pwd1)
-		) {
-			errs.pwd1 =
-				'비밀번호는 6글자 이상, 영문, 숫자, 특수문자를 모두 포함하세요';
-		}
-		//pwd2 인증처리
-		if (value.pwd1 !== value.pwd2 || !value.pwd2) {
-			errs.pwd2 = '두개의 비밀번호를 동일하게 입력하세요';
-		}
-		//gender 인증처리
-		if (!value.gender) {
-			errs.gender = '성별을 선택하세요';
-		}
-		//interests 인증처리
-		if (!value.interests) {
-			errs.interests = '관심사를 하나 이상 선택하세요';
-		}
-		//edu 인증처리
-		if (value.edu === '') {
-			errs.edu = '최종학력을 선택하세요';
-		}
-		//comments 인증처리
-		if (value.comments.length < 10) {
-			errs.comments = '남기는 말을 10글자 이상 입력하세요';
-		}
-		return errs;
-	};
-
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		setErr(check(Val));
-	};
-
-	const handleChange = (e) => {
-		const { name, value } = e.target;
-		setVal({ ...Val, [name]: value });
-	};
-
-	const handleRadio = (e) => {
-		const { name } = e.target;
-		const isCheck = e.target.checked;
-		setVal({ ...Val, [name]: isCheck });
-	};
-
-	const handleCheck = (e) => {
-		let isCheck = false;
-		const { name } = e.target;
-		const inputs = e.target.parentElement.querySelectorAll('input');
-
-		inputs.forEach((el) => {
-			if (el.checked) isCheck = true;
-		});
-		setVal({ ...Val, [name]: isCheck });
-	};
-
-	const handleSelect = (e) => {
-		const { name, value } = e.target;
-		setVal({ ...Val, [name]: value });
-	};
-
-	useEffect(() => {
-		//전송 클릭시 에러메세지를 가지고 값이 Err스테이트 객체에 하나도 없으면 인증통과
-		//Objec.keys(확인할 객체) : 특정 객체의 키값을 배열로 반환해주는 객체전용 내장함수
-		const len = Object.keys(Err).length;
-		//에러메세지가 하나도 없고 Submit버튼을 클릭시 두개 조건을 모두 만족해야지 인증성공처리
-		if (len === 0 && Submit) {
-			alert('회원가입이 완료되었습니다. 메인페이지로 이동합니다.');
-			//메인페이지로 강제 이동
-			history.push('/');
-		}
-	}, [Err]);
 
 	return (
 		<Layout name={'Members'}>
-			<form onSubmit={handleSubmit}>
+			<form>
 				<fieldset>
 					<legend>회원가입 폼 양식</legend>
 					<table>
 						<caption>회원가입 정보 입력</caption>
 						<tbody>
-							{/* userid */}
 							<tr>
 								<th scope='row'>
 									<label htmlFor='userid'>USER ID</label>
@@ -131,10 +35,14 @@ function Members() {
 										placeholder='아이디를 입력하세요'
 										name='userid'
 										id='userid'
-										//value={Val.userid}
-										onChange={handleChange}
+										value={Val.userid}
+										onChange={(e) => {
+											console.log(e.target.value);
+											// onchange 이벤트가 발생할때마다 기존의 Val 스테이트값을 복사해서 현재 입력하고 있는 값으로 업데이트
+											setVal({ ...Val, userid: e.target.value });
+										}}
 									/>
-									<span className='err'>{Err.userid}</span>
+									<span className='err'></span>
 								</td>
 							</tr>
 
@@ -148,11 +56,9 @@ function Members() {
 										type='password'
 										name='pwd1'
 										id='pwd1'
-										//value={Val.pwd1}
 										placeholder='비밀번호를 입력하세요'
-										onChange={handleChange}
 									/>
-									<span className='err'>{Err.pwd1}</span>
+									<span className='err'></span>
 								</td>
 							</tr>
 							<tr>
@@ -164,11 +70,9 @@ function Members() {
 										type='password'
 										name='pwd2'
 										id='pwd2'
-										//value={Val.pwd2}
 										placeholder='비밀번호를 재입력하세요'
-										onChange={handleChange}
 									/>
-									<span className='err'>{Err.pwd2}</span>
+									<span className='err'></span>
 								</td>
 							</tr>
 
@@ -182,11 +86,9 @@ function Members() {
 										type='text'
 										id='email'
 										name='email'
-										//value={Val.email}
 										placeholder='이메일 주소를 입력하세요'
-										onChange={handleChange}
 									/>
-									<span className='err'>{Err.email}</span>
+									<span className='err'></span>
 								</td>
 							</tr>
 
@@ -195,21 +97,11 @@ function Members() {
 								<th scope='row'>GENDER</th>
 								<td>
 									<label htmlFor='male'>MALE</label>
-									<input
-										type='radio'
-										id='male'
-										name='gender'
-										onChange={handleRadio}
-									/>
+									<input type='radio' id='male' name='gender' />
 
 									<label htmlFor='female'>FEMALE</label>
-									<input
-										type='radio'
-										id='female'
-										name='gender'
-										onChange={handleRadio}
-									/>
-									<span className='err'>{Err.gender}</span>
+									<input type='radio' id='female' name='gender' />
+									<span className='err'></span>
 								</td>
 							</tr>
 
@@ -218,29 +110,14 @@ function Members() {
 								<th scope='row'>INTERESTS</th>
 								<td>
 									<label htmlFor='sports'>Sports</label>
-									<input
-										type='checkbox'
-										id='sports'
-										name='interests'
-										onChange={handleCheck}
-									/>
+									<input type='checkbox' id='sports' name='interests' />
 
 									<label htmlFor='music'>Music</label>
-									<input
-										type='checkbox'
-										id='music'
-										name='interests'
-										onChange={handleCheck}
-									/>
+									<input type='checkbox' id='music' name='interests' />
 
 									<label htmlFor='game'>Game</label>
-									<input
-										type='checkbox'
-										id='game'
-										name='interests'
-										onChange={handleCheck}
-									/>
-									<span className='err'>{Err.interests}</span>
+									<input type='checkbox' id='game' name='interests' />
+									<span className='err'></span>
 								</td>
 							</tr>
 
@@ -250,14 +127,14 @@ function Members() {
 									<label htmlFor='edu'>EDUCATION</label>
 								</th>
 								<td>
-									<select name='edu' id='edu' onChange={handleSelect}>
+									<select name='edu' id='edu'>
 										<option value=''>최종 학력을 선택하세요</option>
 										<option value='elementary-school'>초등학교 졸업</option>
 										<option value='middle-school'>중학교 졸업</option>
 										<option value='high-school'>고등학교 졸업</option>
 										<option value='college'>대학교 졸업</option>
 									</select>
-									<span className='err'>{Err.edu}</span>
+									<span className='err'></span>
 								</td>
 							</tr>
 
@@ -272,9 +149,8 @@ function Members() {
 										id='comments'
 										cols='30'
 										rows='3'
-										placeholder='남기는말을 입력하세요'
-										onChange={handleChange}></textarea>
-									<span className='err'>{Err.comments}</span>
+										placeholder='남기는말을 입력하세요'></textarea>
+									<span className='err'></span>
 								</td>
 							</tr>
 
@@ -282,11 +158,7 @@ function Members() {
 							<tr>
 								<th colSpan='2'>
 									<input type='reset' value='cancel' />
-									<input
-										type='submit'
-										value='submit'
-										onClick={() => setSubmit(true)}
-									/>
+									<input type='submit' value='submit' />
 								</th>
 							</tr>
 						</tbody>
