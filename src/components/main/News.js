1ac97bf8bcd52ed6b7a6f29807ee1a62dd1b931e
hklim00 <g5bugi82@gmail.com>
2022-08-16 12:42:40 +0900

    news 페이지에 로컬데이터이 불러오기기

--- src/components/main/News.js
+++ src/components/main/News.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 
 function News() {
-	return <div>News</div>;
+	const getLocalData = () => {
+		const dummyPosts = [
+			{ title: 'Hello5', content: 'Here comes description in detail.' },
+			{ title: 'Hello4', content: 'Here comes description in detail.' },
+			{ title: 'Hello3', content: 'Here comes description in detail.' },
+			{ title: 'Hello2', content: 'Here comes description in detail.' },
+			{ title: 'Hello1', content: 'Here comes description in detail.' },
+		];
+		const data = localStorage.getItem('post');
+		if (data) {
+			return JSON.parse(data);
+		} else {
+			return dummyPosts;
+		}
+	};
+	const [Posts, setPosts] = useState(getLocalData());
+
+	useEffect(() => {
+		localStorage.setItem('post', JSON.stringify(Posts));
+	}, []);
+
+	return (
+		<section id='news'>
+			<h1>News</h1>
+			{Posts.map((post, idx) => {
+				if (idx >= 3) return;
+
+				return (
+					<article key={idx}>
+						<h2>{post.title}</h2>
+						<p>{post.content}</p>
+					</article>
+				);
+			})}
+		</section>
+	);
 }
 
 export default News;
